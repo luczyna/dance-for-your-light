ee2367dbeeb7e7aeb0d7777c856e85869ccf7342
Ana Lu <luczynaki@gmail.com>
2018-08-10 23:00:26 -0500

    update the DanceService to check for 3 dances

--- src/services/DanceService.vue
+++ src/services/DanceService.vue
@@ -13,6 +13,20 @@ export default {
   methods: {
     log(which) {
       this.keylog.push(which);
+      this.detectMove();
+    },
+    detectMove() {
+      const matchShimee = this.keylog.slice(-(SHIMEE.length)).toString() === SHIMEE.toString();
+      const matchShake = this.keylog.slice(-(SHAKE.length)).toString() === SHAKE.toString();
+      const matchYolo = this.keylog.slice(-(YOLO.length)).toString() === YOLO.toString();
+
+      if (matchShimee) {
+        this.$emit('danceMatch', 'shimee');
+      } else if (matchShake) {
+        this.$emit('danceMatch', 'shake');
+      } else if (matchYolo) {
+        this.$emit('danceMatch', 'yolo');
+      }
     }
   }
 }
--- tests/unit/DanceService.spec.js
+++ tests/unit/DanceService.spec.js
@@ -2,18 +2,115 @@ import { shallowMount } from '@vue/test-utils';
 import Home from '@/views/Home.vue';
 
 describe('Dancing at Home', () => {
+  let service;
+
+  beforeEach(() => {
+    service = shallowMount(Home);
+  });
+
   it('stores a log of key presses', () => {
-    const service = shallowMount(Home);
     service.vm.log('a');
     service.vm.log('b');
     service.vm.log('c');
     service.vm.log('d');
-    
+
     expect(service.vm.keylog.length).not.toBe(0);
 
     expect(service.vm.keylog[0]).toBe('a');
     expect(service.vm.keylog[1]).toBe('b');
     expect(service.vm.keylog[2]).toBe('c');
     expect(service.vm.keylog[3]).toBe('d');
+  });
+
+  describe('the SHIMEE move', () => {
+    it('detects the move by itself', () => {
+      service.vm.log('a');
+      service.vm.log('a');
+      service.vm.log('b');
+
+      expect(service.emitted().danceMatch).toBeDefined();
+      expect(service.emitted().danceMatch[0][0]).toBe('shimee');
+    });
+
+    it('detects the move when preceeded with other moves', () => {
+      service.vm.log('d');
+      service.vm.log('a');
+      service.vm.log('a');
+      service.vm.log('a');
+      service.vm.log('b');
+
+      expect(service.emitted().danceMatch).toBeDefined();
+      expect(service.emitted().danceMatch[0][0]).toBe('shimee');
+    });
+
+    it('ignores mismatches to the SHIMEE', () => {
+      service.vm.log('a');
+      service.vm.log('a');
+      service.vm.log('d');
+
+      expect(service.emitted().danceMatch).not.toBeDefined();
+    });
+  });
+
+  describe('the SHAKE move', () => {
+    it('detects the move by itself', () => {
+      service.vm.log('c');
+      service.vm.log('a');
+      service.vm.log('c');
+
+      expect(service.emitted().danceMatch).toBeDefined();
+      expect(service.emitted().danceMatch[0][0]).toBe('shake');
+    });
+
+    it('detects the move when preceeded with other moves', () => {
+      service.vm.log('d');
+      service.vm.log('a');
+      service.vm.log('c');
+      service.vm.log('a');
+      service.vm.log('c');
+
+      expect(service.emitted().danceMatch).toBeDefined();
+      expect(service.emitted().danceMatch[0][0]).toBe('shake');
+    });
+
+    it('ignores mismatches to the SHAKE', () => {
+      service.vm.log('c');
+      service.vm.log('a');
+      service.vm.log('d');
+
+      expect(service.emitted().danceMatch).not.toBeDefined();
+    });
+  });
+
+  describe('the YOLO move', () => {
+    it('detects the move by itself', () => {
+      service.vm.log('d');
+      service.vm.log('a');
+      service.vm.log('a');
+      service.vm.log('c');
+
+      expect(service.emitted().danceMatch).toBeDefined();
+      expect(service.emitted().danceMatch[0][0]).toBe('yolo');
+    });
+
+    it('detects the move when preceeded with other moves', () => {
+      service.vm.log('d');
+      service.vm.log('d');
+      service.vm.log('a');
+      service.vm.log('a');
+      service.vm.log('c');
+
+      expect(service.emitted().danceMatch).toBeDefined();
+      expect(service.emitted().danceMatch[0][0]).toBe('yolo');
+    });
+
+    it('ignores mismatches to the YOLO', () => {
+      service.vm.log('b');
+      service.vm.log('a');
+      service.vm.log('a');
+      service.vm.log('c');
+
+      expect(service.emitted().danceMatch).not.toBeDefined();
+    });
   });
 });
