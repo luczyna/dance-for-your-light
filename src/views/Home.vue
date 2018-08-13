61b08908d9825e98c74885a9cce5cdc70ca2a2ac
Ana Lu <luczynaki@gmail.com>
2018-08-12 21:13:48 -0500

    update image use

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="page">
+    <p class="centered"><img src="img/dance-for-your-life-luczynski.PNG" alt="don't get crushed without your light" class="centered-image" /></p>
     <h1 class="centered">Dance for your Light</h1>
 
     <p class="centered"><router-link :to="{name: 'learn'}">learn</router-link> or <router-link :to="{name: 'game'}">get dancing</router-link></p>
